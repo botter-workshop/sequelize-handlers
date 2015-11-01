309d59dbabbf2b2e13c6b623ff2d891720f5c04e
Ben Hanna <ben@ducky-pond.com>
2015-11-01 17:59:11 +0000

    Added qs parser implementation.

---
+++ parsers/qs.js
@@ -0,0 +1,65 @@
+var _ = require('lodash');
+
+var qs = {};
+
+qs.limit = function (value) {
+    value = parseInt(value);
+    
+    if (!value || value < 0) {
+        value = 0;
+    }    
+    return value;
+};
+
+qs.offset = function (value) {
+    value = parseInt(value);
+    
+    if (!value || value < 0) {
+        value = 0;
+    }
+    return value;
+};
+
+qs.sort = function (options) {
+    var properties,
+        sort = null;
+        
+    if (options) {
+        properties = options.split(',');
+        
+        sort = _.map(properties, function (x) {
+            if (x.indexOf('-') === 0) {
+                return [x.substr(1), 'DESC'];
+            } else {
+                return [x, 'ASC'];
+            }
+        });
+    }
+    
+    return sort;
+};
+
+qs.fields = function (options) {
+    var fields = null;
+    
+    if (options) {
+        fields = options.split(',');
+    }
+
+    return fields;
+};
+    
+qs.filters = function (options) {
+    var filters = null;
+    
+    if (!_.isEmpty(options)) {
+        filters = {};
+        _.forOwn(options, function (value, key) {
+            filters[key] = value.split(',');
+        });
+    }
+    
+    return filters;
+};
+
+module.exports = qs;
