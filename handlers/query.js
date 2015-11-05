cd2eb194051779442cc9e470998785a05da28c87
Ben Hanna <ben@ducky-pond.com>
2015-11-05 17:02:13 +0000

    Updated the query handler so that options can be overridden.

--- handlers/query.js
+++ handlers/query.js
@@ -5,39 +5,38 @@ module.exports = function (model) {
     var middlewares = [];
     
     middlewares.push(function (req, res, next) {
-        var query = {},
+        var options = {},
             keys = {};
         
         keys.model = _.keys(model.rawAttributes);
         keys.query = _.keys(req.query);
         keys.filters = _.intersection(keys.model, keys.query);
         
-        query.attributes = qs.fields(req.query.fields);
-        query.limit = qs.limit(req.query.limit) || 50;
-        query.offset = qs.offset(req.query.offset);
-        query.order = qs.sort(req.query.sort);
-        query.where = qs.filters(_.pick(req.query, keys.filters));
+        options.attributes = qs.fields(req.query.fields);
+        options.limit = qs.limit(req.query.limit) || 50;
+        options.offset = qs.offset(req.query.offset);
+        options.order = qs.sort(req.query.sort);
+        options.where = qs.filters(_.pick(req.query, keys.filters));
         
-        query = _.omit(query, _.isNull);
+        options = _.omit(options, _.isNull);
         
-        req.locals = req.locals || {};
-        req.locals.query = query;
+        req.options = _.assign({}, options, req.options);
         
         next();
     });
     
     middlewares.push(function (req, res, next) {
-        var query = req.locals.query;
+        var options = req.options;
 
         model
-            .findAndCountAll(query)
+            .findAndCountAll(options)
             .then(respond)
             .catch(next);
             
         function respond(result) {
             var count = result.count,
-                start = query.offset, 
-                end = query.offset + query.limit;
+                start = options.offset, 
+                end = options.offset + options.limit;
             
             if (end >= count) {
                 end = count;
