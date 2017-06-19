c56a1637dda6806594e89e8c4eb19a4926aa07ae
Ben Hanna <ben@ducky-pond.com>
2017-06-19 12:29:16 +0000

    Updated get handler to support all primary keys on a model.

--- handlers/get.js
+++ handlers/get.js
@@ -1,4 +1,6 @@
-var HttpStatusError = require('../errors/HttpStatusError'),
+var _ = require('lodash'),
+    qs = require('../parsers/qs'),
+    HttpStatusError = require('../errors/HttpStatusError'),
     plainTransform = require('../transforms/plain');
 
 module.exports = init;
@@ -10,11 +12,17 @@ function init(model) {
     ];
     
     function get(req, res, next) {
-        var options = req.options || {};
-
-        options.where = options.where || {};
-        options.where[model.primaryKeyAttribute] = req.params.id;
-
+        var keys = {},
+            options = {};
+        
+        keys.model = _.keys(model.primaryKeyAttributes);
+        keys.params = _.keys(req.params);
+        keys.filters = _.intersection(keys.model, keys.params);
+    
+        options.where = qs.filters(_.pick(req.params, keys.filters))
+        
+        options = _.merge({}, options, req.options);
+        
         model
             .findOne(options)
             .then(respond)
