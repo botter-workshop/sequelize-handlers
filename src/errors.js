8e53d98edb106405a82e118c108b7ccd39a3de3a
Ben Hanna <ben@ducky-pond.com>
2017-06-21 16:26:22 +0000

    Refactored HttpStatusError into a proper class.

--- src/errors.js
+++ src/errors.js
@@ -1,11 +1,10 @@
-function HttpStatusError(status, message) {
-    this.name = 'HttpStatusError';
-    this.status = status;
-    this.message = message;
+class HttpStatusError extends Error {
+    constructor(status, message) {
+        this.name = 'HttpStatusError';
+        this.status = status;
+        this.message = message;
+    }
 }
-
-HttpStatusError.prototype = Object.create(Error.prototype);
-HttpStatusError.prototype.constructor = HttpStatusError;
 
 module.exports = {
     HttpStatusError
