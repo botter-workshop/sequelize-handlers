5a8c87e2861c4b56dde88856417c3ab6fdc9a818
DhanyaHerath <dhanya@Dhanyas-MacBook-Pro.local>
2017-08-29 16:52:01 +1200

    Add response headers for query method

--- src/handler.js
+++ src/handler.js
@@ -8,74 +8,76 @@ class ModelHandler {
         this.model = model;
         this.defaults = defaults;
     }
-    
+
     create() {
         const handle = (req, res, next) => {
             this.model
                 .create(req.body)
                 .then(respond)
                 .catch(next);
-            
+
             function respond(row) {
                 res.status(201);
                 res.send(res.transform(row));
             }
         };
-        
+
         return [
             raw,
             handle
         ];
     }
-    
+
     get() {
         const handle = (req, res, next) => {
             this
                 .findOne(req.params)
                 .then(respond)
                 .catch(next);
-            
+
             function respond(row) {
                 if (!row) {
                     throw new HttpStatusError(404, 'Not Found');
                 }
-                
+
                 res.send(res.transform(row));
             }
         };
-        
+
         return [
             raw,
             handle
         ];
     }
-    
+
     query() {
         const handle = (req, res, next) => {
             this
                 .findAndCountAll(req.query)
                 .then(respond)
                 .catch(next);
-            
+
             function respond({ rows, start, end, count }) {
                 res.set('Content-Range', `${start}-${end}/${count}`);
-                
+
                 if (count > end) {
                     res.status(206);
                 } else {
                     res.status(200);
                 }
-                
+
                 res.send(res.transform(rows));
+                res.setHeader('Access-Control-Expose-Headers', 'X-Total-Count');
+                res.setHeader('X-Total-Count', count);
             }
         };
-        
+
         return [
             raw,
             handle
         ];
     }
-    
+
     remove() {
         const handle = (req, res, next) => {
             this
@@ -83,25 +85,25 @@ class ModelHandler {
                 .then(destroy)
                 .then(respond)
                 .catch(next);
-            
+
             function destroy(row) {
                 if (!row) {
                     throw new HttpStatusError(404, 'Not Found');
                 }
-                
+
                 return row.destroy();
             }
-            
+
             function respond() {
                 res.sendStatus(204);
             }
         };
-        
+
         return [
             handle
         ];
     }
-    
+
     update() {
         const handle = (req, res, next) => {
             this
@@ -109,48 +111,48 @@ class ModelHandler {
                 .then(updateAttributes)
                 .then(respond)
                 .catch(next);
-                
+
             function updateAttributes(row) {
                 if (!row) {
                     throw new HttpStatusError(404, 'Not Found');
                 }
-                
+
                 return row.updateAttributes(req.body);
             }
-            
+
             function respond(row) {
                 res.send(res.transform(row));
             }
         };
-        
+
         return [
             raw,
             handle
         ];
     }
-    
+
     findOne(params, options) {
         options = _.merge(parse(params, this.model), options);
 
         return this.model.findOne(options);
     }
-    
+
     findAndCountAll(params, options) {
         let parsed = parse(params, this.model);
-        
+
         options = _(parsed)
             .defaults(this.defaults)
             .merge(options)
             .value();
-        
+
         return this.model
             .findAndCountAll(options)
             .then(extract);
-            
+
         function extract({ count, rows }) {
             const start = options.offset;
             const end = Math.min(count, (options.offset + options.limit) || count);
-        
+
             return { rows, start, end, count };
         }
     }
