4375a6a8ba2549e3d0f465610eb848598d56fb25
Sean Ray <sean.ray@wwt.com>
2018-01-11 14:14:57 -0500

    Expose `parse` and `HttpStatusError` on exports
    This makes it possible to override individual handler functions
    while still being able to take advantage of all of the url parsing
    logic

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const ModelHandler = require('./src/handler');
+const { HttpStatusError } = require('./src/errors');
+const { parse } = require('./src/parser');
 
 module.exports = {
-    ModelHandler
+    ModelHandler,
+    HttpStatusError,
+    parse
 };
